82e5424896ba4a3d04c9ad535b5fda865b453a5d
Dustin Woodard <dustin.woodard@gmail.com>
2025-07-13 16:45:35 -0600

    Fix service provider config loading - use default config instead of requiring file

--- src/Laravel/Neo4jEloquentServiceProvider.php
+++ src/Laravel/Neo4jEloquentServiceProvider.php
@@ -13,10 +13,20 @@ class Neo4jEloquentServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/../../../config/neo4j.php',
-            'neo4j'
-        );
+        // Merge default config
+        $this->app['config']->set('neo4j', array_merge([
+            'connections' => [
+                'default' => [
+                    'uri' => env('NEO4J_URI', 'bolt://localhost:7687'),
+                    'username' => env('NEO4J_USERNAME', 'neo4j'),
+                    'password' => env('NEO4J_PASSWORD', 'password'),
+                    'database' => env('NEO4J_DATABASE', 'neo4j'),
+                ],
+            ],
+            'default' => env('NEO4J_CONNECTION', 'default'),
+            'log_queries' => env('NEO4J_LOG_QUERIES', false),
+            'auto_uuid' => env('NEO4J_AUTO_UUID', true),
+        ], $this->app['config']->get('neo4j', [])));
 
         $this->app->singleton(Neo4jService::class, function ($app) {
             return new Neo4jService($app['config']['neo4j']);
@@ -30,10 +40,14 @@ class Neo4jEloquentServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Publish config file if running in console
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../../../config/neo4j.php' => config_path('neo4j.php'),
-            ], 'neo4j-config');
+            $configPath = __DIR__.'/../../config/neo4j.php';
+            if (file_exists($configPath)) {
+                $this->publishes([
+                    $configPath => config_path('neo4j.php'),
+                ], 'neo4j-config');
+            }
         }
 
         // FIXED: Set the Neo4j service on the Node class
